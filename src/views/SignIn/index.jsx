60139060495dcb02f1f7ae76eca1bc33588a3c63
Leonardo Machado <leonardoms@id.uff.br>
2019-07-15 23:43:33 -0300

    login screen with logo centered and minimalist

--- src/views/SignIn/index.jsx
+++ src/views/SignIn/index.jsx
@@ -14,17 +14,10 @@ import { withStyles } from '@material-ui/core';
 import {
   Grid,
   Button,
-  IconButton,
   CircularProgress,
   TextField,
   Typography
 } from '@material-ui/core';
-
-// Material icons
-import { ArrowBack as ArrowBackIcon } from '@material-ui/icons';
-
-// Shared components
-import { Facebook as FacebookIcon, Google as GoogleIcon } from 'icons';
 
 // Component styles
 import styles from './styles';
@@ -129,94 +122,31 @@ class SignIn extends Component {
           container
         >
           <Grid
-            className={classes.quoteWrapper}
-            item
-            lg={5}
-          >
-            <div className={classes.quote}>
-              <div className={classes.quoteInner}>
-                <Typography
-                  className={classes.quoteText}
-                  variant="h1"
-                >
-                  Hella narwhal Cosby sweater McSweeney's, salvia kitsch before
-                  they sold out High Life.
-                </Typography>
-                <div className={classes.person}>
-                  <Typography
-                    className={classes.name}
-                    variant="body1"
-                  >
-                    Takamaru Ayako
-                  </Typography>
-                  <Typography
-                    className={classes.bio}
-                    variant="body2"
-                  >
-                    Manager at inVision
-                  </Typography>
-                </div>
-              </div>
-            </div>
-          </Grid>
-          <Grid
             className={classes.content}
             item
-            lg={7}
+            lg={12}
             xs={12}
           >
             <div className={classes.content}>
-              <div className={classes.contentHeader}>
-                <IconButton
-                  className={classes.backButton}
-                  onClick={this.handleBack}
-                >
-                  <ArrowBackIcon />
-                </IconButton>
+              <div className={classes.contentHeader}>              
               </div>
               <div className={classes.contentBody}>
+              <div className={classes.empty}>
+            
+          </div>
                 <form className={classes.form}>
-                  <Typography
-                    className={classes.title}
-                    variant="h2"
-                  >
-                    Sign in
-                  </Typography>
-                  <Typography
-                    className={classes.subtitle}
-                    variant="body1"
-                  >
-                    Sign in with social media
-                  </Typography>
-                  <Button
-                    className={classes.facebookButton}
-                    color="primary"
-                    onClick={this.handleSignIn}
-                    size="large"
-                    variant="contained"
-                  >
-                    <FacebookIcon className={classes.facebookIcon} />
-                    Login with Facebook
-                  </Button>
-                  <Button
-                    className={classes.googleButton}
-                    onClick={this.handleSignIn}
-                    size="large"
-                    variant="contained"
-                  >
-                    <GoogleIcon className={classes.googleIcon} />
-                    Login with Google
-                  </Button>
-                  <Typography
-                    className={classes.sugestion}
-                    variant="body1"
-                  >
-                    or login with email address
-                  </Typography>
+                
+                <div className={classes.contentBody}>
+              <img
+                alt="Empty list"
+                className={classes.emptyImage}
+                src="/images/logo.png"
+              />
+            </div>                 
                   <div className={classes.fields}>
                     <TextField
                       className={classes.textField}
-                      label="Email address"
+                      label="Email"
                       name="email"
                       onChange={event =>
                         this.handleFieldChange('email', event.target.value)
@@ -235,7 +165,7 @@ class SignIn extends Component {
                     )}
                     <TextField
                       className={classes.textField}
-                      label="Password"
+                      label="Senha"
                       name="password"
                       onChange={event =>
                         this.handleFieldChange('password', event.target.value)
@@ -272,19 +202,19 @@ class SignIn extends Component {
                       size="large"
                       variant="contained"
                     >
-                      Sign in now
+                      Entrar
                     </Button>
                   )}
                   <Typography
                     className={classes.signUp}
                     variant="body1"
                   >
-                    Don't have an account?{' '}
+                    Ainda não tem uma conta?{' '}
                     <Link
                       className={classes.signUpUrl}
                       to="/sign-up"
                     >
-                      Sign up
+                      Registre-se
                     </Link>
                   </Typography>
                 </form>
--- src/views/SignIn/styles.jsx
+++ src/views/SignIn/styles.jsx
@@ -31,7 +31,7 @@ export default theme => ({
     fontWeight: 300
   },
   name: {
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     color: theme.palette.common.white
   },
   bio: {
@@ -46,49 +46,47 @@ export default theme => ({
   contentHeader: {
     display: 'flex',
     alignItems: 'center',
-    paddingTop: theme.spacing.unit * 5,
-    paddingBototm: theme.spacing.unit * 2,
-    paddingLeft: theme.spacing.unit * 2,
-    paddingRight: theme.spacing.unit * 2
+    paddingTop: theme.spacing(5),
+    paddingBototm: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2)
   },
   backButton: {},
   logoImage: {
-    marginLeft: theme.spacing.unit * 4
+    marginLeft: theme.spacing(4)
   },
   contentBody: {
     flexGrow: 1,
     display: 'flex',
     alignItems: 'center',
-    [theme.breakpoints.down('md')]: {
-      justifyContent: 'center'
-    }
-  },
+    justifyContent: 'center'
+  }, 
   form: {
     paddingLeft: '100px',
     paddingRight: '100px',
     paddingBottom: '125px',
     flexBasis: '700px',
     [theme.breakpoints.down('sm')]: {
-      paddingLeft: theme.spacing.unit * 2,
-      paddingRight: theme.spacing.unit * 2
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2)
     }
   },
   title: {
-    marginTop: theme.spacing.unit * 3
+    marginTop: theme.spacing(3)
   },
   subtitle: {
     color: theme.palette.text.secondary,
-    marginTop: theme.spacing.unit * 0.5
+    marginTop: theme.spacing(0.5)
   },
   facebookButton: {
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     width: '100%'
   },
   facebookIcon: {
     marginRight: theme.spacing.unit
   },
   googleButton: {
-    marginTop: theme.spacing.unit * 2,
+    marginTop: theme.spacing(2),
     width: '100%'
   },
   googleIcon: {
@@ -96,16 +94,16 @@ export default theme => ({
   },
   sugestion: {
     color: theme.palette.text.secondary,
-    marginTop: theme.spacing.unit * 2,
+    marginTop: theme.spacing(2),
     textAlign: 'center'
   },
   fields: {
-    marginTop: theme.spacing.unit * 2
+    marginTop: theme.spacing(2)
   },
   textField: {
     width: '100%',
     '& + & ': {
-      marginTop: theme.spacing.unit * 2
+      marginTop: theme.spacing(2)
     }
   },
   policy: {
@@ -128,16 +126,16 @@ export default theme => ({
   },
   progress: {
     display: 'block',
-    marginTop: theme.spacing.unit * 2,
+    marginTop: theme.spacing(2),
     marginLeft: 'auto',
     marginRight: 'auto'
   },
   signInButton: {
-    marginTop: theme.spacing.unit * 2,
+    marginTop: theme.spacing(2),
     width: '100%'
   },
   signUp: {
-    marginTop: theme.spacing.unit * 2,
+    marginTop: theme.spacing(2),
     color: theme.palette.text.secondary
   },
   signUpUrl: {
@@ -149,13 +147,13 @@ export default theme => ({
   },
   fieldError: {
     color: theme.palette.danger.main,
-    marginBottom: theme.spacing.unit * 2,
+    marginBottom: theme.spacing(2),
     marginTop: theme.spacing.unit
   },
   submitError: {
     color: theme.palette.danger.main,
     alignText: 'center',
     marginBottom: theme.spacing.unit,
-    marginTop: theme.spacing.unit * 2
+    marginTop: theme.spacing(2)
   }
 });
